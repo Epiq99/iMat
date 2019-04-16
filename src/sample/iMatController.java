0c9e5cc312118742411e192462e7cfddc3ba6f7f
Epiq99 <damoller99@hotmail.se>
2019-04-16 12:06:38 +0200

    Some work

--- src/sample/iMatController.java
+++ src/sample/iMatController.java
@@ -1,5 +1,11 @@
 package sample;
+import se.chalmers.cse.dat216.project.*;
 
 public class iMatController {
+    public void test() {
+        IMatDataHandler handler = IMatDataHandler.getInstance();
 
+        System.out.println(handler.getProducts().get(0).getImageName());
+
+    }
 }
