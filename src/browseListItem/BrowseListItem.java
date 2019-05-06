bc3d5f3363b07469fdd20adc442992dbb922604c
Epiq99 <damoller99@hotmail.se>
2019-05-06 16:22:49 +0200

    Skapade en bas att fortsätta från

--- src/browseListItem/BrowseListItem.java
+++ src/browseListItem/BrowseListItem.java
@@ -1,4 +1,41 @@
 package browseListItem;
 
-public class BrowseListItem {
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.*;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
+public class BrowseListItem extends AnchorPane {
+
+    private final String imageFolderPath = "images/";
+
+    @FXML Label itemNameLable;
+    @FXML Label priceLable;
+    @FXML Label unitLable;
+    @FXML ImageView itemImage;
+
+    public BrowseListItem(Product prod){
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("browselistitem.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        itemNameLable.setText(prod.getName());
+        priceLable.setText(String.valueOf(prod.getPrice()));
+        unitLable.setText(prod.getUnit());
+        itemImage.setImage(new Image(imageFolderPath + prod.getImageName()));
+    }
 }
--- src/iMatController.java
+++ src/iMatController.java
@@ -1,10 +1,25 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
 import se.chalmers.cse.dat216.project.*;
+import browseListItem.BrowseListItem;
 
-public class iMatController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class iMatController implements Initializable {
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+
+    @FXML TilePane browserPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for(Product p: handler.getProducts())
+            browserPane.getChildren().add(new BrowseListItem(p));
+    }
+
     public void test() {
-        IMatDataHandler handler = IMatDataHandler.getInstance();
-
-        System.out.println(handler.getProducts().get(0).getImageName());
 
     }
 }
